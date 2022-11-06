7ff15d878ac8411fb125d3bde4d991e7f7cc7720
aleksus1 <116866626+aleksus1@users.noreply.github.com>
2022-11-06 21:45:03 +0300

    Add files via upload
    Пока что шарики стоят, но реализован подсчёт очков за различные цели

--- Поймай шарик.py
+++ Поймай шарик.py
@@ -5,7 +5,7 @@ from random import randint
 
 pg.init()
 
-FPS = 2
+FPS =1
 screen = pg.display.set_mode((1200, 800))
 
 RED = (255, 0, 0)
@@ -30,7 +30,7 @@ def new_ball():  # Рисуем шарик
     global x, y, r, color
     x = randint(100, 1100)
     y = randint(100, 700)
-    r = randint(10, 100)
+    r = randint(5, 100)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
@@ -45,16 +45,25 @@ def multipul_balls():  # Рисуем несколько шариков, с во
     radius.append(r)
 
 
-def ball_death(): #Удаляем самый ранее появившийся шарик на экране
+
+def ball_death(n): #Удаляем самый ранее появившийся шарик на экране
     global number_of_balls, absis, ordinate, radius, FPS, colour
     screen.fill(BLACK)
-    for i in range(1, number_of_balls):
+    absis = absis[:n] + absis[n+1:]
+    ordinate = ordinate[:n] + ordinate[n+1:]
+    radius = radius[:n] + radius[n+1:]
+    colour = colour[:n] + colour[n+1:]
+    for i in range(number_of_balls-1):
         circle(screen, colour[i], (absis[i], ordinate[i]), radius[i])
-    absis = absis[1:]
-    ordinate = ordinate[1:]
-    radius = radius[1:]
-    colour = colour[1:]
 
+
+def score_points(rad):
+    global counter
+    if rad>50:
+        counter+=1
+    elif rad>30: counter+=2
+    elif rad>10: counter+=4
+    else: counter+=8
 
 
 pg.display.update()
@@ -63,6 +72,15 @@ finished = False
 
 while not finished:
     clock.tick(1000)
+
+    multipul_balls()
+    if repeat < number_of_balls:  # Проверка наличия на экране необходимого числа шариков(один шарик появляется за одно повторение)
+        repeat += 1
+    else:
+        ball_death(0)
+        pg.display.update()
+        clock.tick(FPS)
+
     for event in pg.event.get():
         not_miss = False
         if event.type == pg.QUIT:
@@ -73,21 +91,19 @@ while not finished:
 
             for i in range(number_of_balls):
                 if (absis[i] - mousepos[0]) ** 2 + (ordinate[i] - mousepos[1]) ** 2 <= radius[i] ** 2:  # Проверка попадания в один из кругов
-                    counter += 1  # Подсчёт оков
+                    score_points(radius[i])  # Подсчёт оков
                     print("Tap")
                     not_miss = True
+                    ball_death(i) #Удаление нажатого шарика
+                    multipul_balls()
+                    pg.display.update()
             if not not_miss:
                 print("Miss")
-                misses += 1  # Подсчёт промахов`
+                misses += 1  # Подсчёт промахов
                 not_miss = 0
             if misses == 5: finished = True  # Проигрыш при пяти промахах
-    multipul_balls()
-    if repeat < number_of_balls: #Проверка наличия на экране необходимого числа шариков(один шарик появляется за одно повторение)
-        repeat +=1
-        screen.fill(BLACK)
-    else:
-        ball_death()
-        clock.tick(FPS)
-    pg.display.update()
-print("Your count is", counter)
+
+
+
+print("Your score is", counter)
 pg.quit()
